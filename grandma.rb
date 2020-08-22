1bd0a4382af646b10d1e11ba6c6acdd9dfe45b95
Tristan Catron <trilocat@gmail.com>
2020-08-22 05:15:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+  "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase== "WHAT?"
+  "NO, NOT SINCE 1938!"
+end
+end
   
